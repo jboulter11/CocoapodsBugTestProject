bfd64e6408ba76d9b56a8e3574a85d1795d8795b
Jim Boulter <jboulter11@gmail.com>
2020-05-06 20:23:02 -0700

    Initial Commit

---
+++ CocoapodsBugTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocoapodsBugTestProject
+//
+//  Created by James Boulter on 5/6/20.
+//  Copyright © 2020 James Boulter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
